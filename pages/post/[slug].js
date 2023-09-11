dfa512c41643cce728f0d41bf87511fb109d2a63
KristenRoggero <125401655+KristenRoggero@users.noreply.github.com>
2023-09-11 15:04:26 -0500

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -19,13 +19,13 @@ const PostDetails = ({ post }) => {
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">
-              <PostWidget
-                slug={post.slug}
-                categories={post.categories.map((category) => category.slug)}
-              />
+              <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
               <Categories />
             </div>
           </div>
@@ -34,7 +34,6 @@ const PostDetails = ({ post }) => {
     </>
   );
 };
-
 export default PostDetails;
 
 export async function getStaticProps({ params }) {
